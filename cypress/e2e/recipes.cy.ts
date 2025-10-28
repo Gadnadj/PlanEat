fe31d9ff7be0d18173ec7ecd04d8b9d7938b61e1
Gad Nadjar <gadnadjar@gmail.com>
2025-10-28 13:12:26 +0200

    Complete recipe tests with search, filters, and navigation functionality

--- cypress/e2e/recipes.cy.ts
+++ cypress/e2e/recipes.cy.ts
@@ -2,18 +2,12 @@ describe('Recipe Management', () => {
   describe('Unauthenticated User', () => {
     it('should redirect to login when accessing protected recipe pages', () => {
       // Try to access recipes page without auth
-      cy.visit('/recipes')
+      cy.visit('/recipe')
       
       // Should redirect to login
       cy.url().should('include', '/login')
     })
 
-    it('should display recipes on landing page without authentication', () => {
-      cy.visit('/')
-      
-      // Should show recipe section
-      cy.contains('Discover our recipes').should('be.visible')
-    })
   })
 
   describe('Authenticated User', () => {
@@ -23,26 +17,82 @@ describe('Recipe Management', () => {
     })
 
     it('should access recipes page when authenticated', () => {
-      cy.visit('/recipes')
+      cy.visit('/recipe')
       
       // Should be able to access the page
-      cy.url().should('include', '/recipes')
-      // TODO: Vérifier le contenu de la page quand les data-cy seront ajoutés
+      cy.url().should('include', '/recipe')
+      
+      // Should display the header
+      cy.contains('Discover our recipes').should('be.visible')
+      cy.contains('Over 1000 recipes').should('be.visible')
     })
 
-    it('should display recipe list', () => {
-      cy.visit('/recipes')
+    it('should display search and filter elements', () => {
+      cy.visit('/recipe')
       
-      // Vérifier que la page se charge
-      cy.contains('Recipes').should('be.visible')
+      // Check search bar
+      cy.get('input[placeholder*="Search for a recipe"]').should('be.visible')
+      
+      // Check reset button
+      cy.contains('button', 'Reset').should('be.visible')
+      
+      // Check filter buttons exist
+      cy.contains('Category').should('be.visible')
+      cy.contains('Prep Time').should('be.visible')
+      cy.contains('Difficulty').should('be.visible')
     })
 
-    it('should navigate to add recipe page', () => {
-      cy.visit('/recipes')
+    it('should be able to search for recipes', () => {
+      cy.visit('/recipe')
       
-      // TODO: Cliquer sur le bouton "Add Recipe" quand data-cy sera ajouté
-      // cy.get('[data-cy="add-recipe-button"]').click()
-      // cy.url().should('include', '/recipes/new')
+      // Type in search box
+      cy.get('input[placeholder*="Search for a recipe"]').type('pasta')
+      
+      // The input should have the value
+      cy.get('input[placeholder*="Search for a recipe"]').should('have.value', 'pasta')
+    })
+
+    it('should be able to reset filters', () => {
+      cy.visit('/recipe')
+      
+      // Type something in search
+      cy.get('input[placeholder*="Search for a recipe"]').type('test')
+      
+      // Click reset button
+      cy.contains('button', 'Reset').click()
+      
+      // Search should be cleared
+      cy.get('input[placeholder*="Search for a recipe"]').should('have.value', '')
+    })
+
+    it('should display recipe cards when recipes exist', () => {
+      cy.visit('/recipe')
+      
+      // Wait for recipes to load
+      cy.wait(2000)
+      
+      // Should have some recipe content (this will depend on your data)
+      // Just check that the page is not empty
+      cy.get('body').should('not.contain', 'No recipes found')
+    })
+
+    it('should be able to click on a recipe to view details', () => {
+      cy.visit('/recipe')
+      
+      // Wait for recipes to load
+      cy.wait(2000)
+      
+      // Try to click on first recipe card (if exists)
+      // This is a generic test that will work if there are any recipes
+      cy.get('body').then(($body) => {
+        if ($body.text().includes('View Recipe') || $body.find('a[href*="/recipe/"]').length > 0) {
+          // If there's a recipe link, click it
+          cy.get('a[href*="/recipe/"]').first().click()
+          
+          // Should navigate to recipe detail page
+          cy.url().should('match', /\/recipe\/[^/]+$/)
+        }
+      })
     })
   })
 })
